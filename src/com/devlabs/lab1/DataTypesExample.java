72bacf15f4cab867ea40260b354dcc46800209b0
SharmaDeep <SharmaDeep@HBADT4500713.bsg.ad.adp.com>
2020-12-23 21:26:49 +0530

    CoreJavaAssignments and Lab Exercises Commit1

--- src/com/devlabs/lab1/DataTypesExample.java
+++ src/com/devlabs/lab1/DataTypesExample.java
@@ -3,20 +3,40 @@ package com.devlabs.lab1;
 public class DataTypesExample {
 
 	public static void main(String[] args) {
-		int item_id, quantity;
-	    //int quantity; //default value = 0
-	    
-	    double price = 0,discount,total_amt=0; 
-	    
-	    boolean memberCheck; //false
-	    
-	    String comments = "Welcome again!"; //null
-	    
-	    char c = '&'; //single quotes
-	    
+		{
+
+			
+
+	        int itemA=500, itemB=70, itemC=100;
+
+	       
+
+	        double discount=0.15,total_amt;
+
+	        
+
+	        total_amt=((3*itemA)+(5*itemB)+(2*itemC));
+
+	        
+
+	        total_amt = total_amt - discount*total_amt;
+
+	       // total_amt-= (0.15*total_amt);
+
+	        
+
+	        total_amt+=(0.02*total_amt);
+
+	        
+
+	       System.out.println("The total amount is " + total_amt);
+
+		}    
+
+	}
 	    
 	}
 
-	}
+	
 
 
--- src/com/devlabs/lab1/QuotientNRemainderOfNumber.java
+++ src/com/devlabs/lab1/QuotientNRemainderOfNumber.java
@@ -5,6 +5,8 @@ public class QuotientNRemainderOfNumber {
 	public static void main(String[] args) {
 	
 		int number=50;
+		double quo, rem;
+		
 		
 
 	}
